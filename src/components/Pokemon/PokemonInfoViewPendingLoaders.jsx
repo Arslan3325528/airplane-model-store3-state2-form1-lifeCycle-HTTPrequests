15d0a12648282d317414396c1525f2931feddd17
Ruslan379 <3325528@ukr.net>
2026-09-14 16:17:17 +0300

    New STEP 300-45

---
+++ src/components/Pokemon/PokemonInfoViewPendingLoaders.jsx
@@ -0,0 +1,70 @@
+import { PokemonInfoViewData } from './PokemonInfoViewData.jsx';
+import pendingImage from './pending.jpg';
+
+//! + Бібліотеки з Loaders (спінерами)
+import { ImSpinner } from 'react-icons/im';
+import { ClipLoader } from "react-spinners";
+
+import css from "./PokemonInfoViewPending.module.css";
+
+
+export function PokemonInfoViewPendingLoaders({ pokemonName }) {
+  //! Для React-skeleton
+  const pokemonSkeleton = {
+    name: pokemonName,
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: pendingImage,
+        },
+      },
+    },
+    //! Додатковий список властивостей покемона
+    // stats: [],
+    stats: [
+      {
+        stat: { name: "hp" },
+        base_stat: "⏳"
+      },
+      {
+        stat: { name: "attack" },
+        base_stat: "⏳"
+      },
+      {
+        stat: { name: "defense" },
+        base_stat: "⏳"
+      },
+      {
+        stat: { name: "special-attack" },
+        base_stat: "⏳"
+      },
+      {
+        stat: { name: "special-defense" },
+        base_stat: "⏳"
+      },
+      {
+        stat: { name: "speed" },
+        base_stat: "⏳"
+      }
+    ],
+  };
+
+  return (
+    <div role="alert">
+      <h2><u><i>Ви ввели ім'я покемона</i></u>: <b>{pokemonName}</b></h2>
+      <div className={css.spinnerBox}>
+        {/* //! Loader-іконка */}
+        {/* <ImSpinner size="36" className={css.iconSpin} />  */}
+        {/* //! Loader: бібліотека react-spinners */}
+        <ClipLoader
+          size={36}
+          color="#19ffff"
+          aria-label="Завантаження покемона"
+        />
+        <h2 className={css.pokemonInfoLoading}>Завантажуємо покемон...</h2>
+      </div>
+      {/* //! React-skeleton (шаблон) */}
+      <PokemonInfoViewData pokemon={pokemonSkeleton} />
+    </div>
+  );
+};
