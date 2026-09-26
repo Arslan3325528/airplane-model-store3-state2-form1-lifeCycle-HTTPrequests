d2704b2b916d804b74964c94b5b183c81695a01f
Ruslan379 <3325528@ukr.net>
2026-09-27 00:26:00 +0300

    New STEP 300-71

--- src/components/RegistrationIdentification/RegistrationIdentification-Class.jsx
+++ src/components/RegistrationIdentification/RegistrationIdentification-Class.jsx
@@ -22,17 +22,15 @@ export class RegistrationIdentification extends Component {
         activeUser: nextActiveUser,
         activeUserName: nextActiveUser.userName
       });
-
     };
   };
 
   render() {
-
     const {
       activeUser, //! 🗣 активний (авторизований) користувач
       activeUserName //! Ім'я Активного користувача
     } = this.state;
-    
+
     const {
       onClose, //! відкриття/закриття модального вікна
       // activeUser, //! 🗣 активний (авторизований) користувач
@@ -49,7 +47,7 @@ export class RegistrationIdentification extends Component {
 
     return (
       <div className={css.boxRegistrationIdentification}>
-        {!loader
+        {activeUser
           ?
           <h2 className={css.titleRegistrationIdentification}>
             Вітаю вас, <span className={css.titleUserRegistrationIdentification}>{activeUserName}</span>
@@ -100,66 +98,3 @@ export class RegistrationIdentification extends Component {
     );
   };
 };
-
-
-//? Підняття стану
-//! Звичайний компонент
-// export function RegistrationIdentification({
-//   onClose, //! відкриття/закриття модального вікна
-//   activeUser, //! 🗣 активний (авторизований) користувач
-//   onSignOut, //! завершення сеансу облікового запису
-//   loader, //! ⏳ індикатор завантаження (лоадер)
-// }) {
-//   console.log("RegistrationIdentification 🗣 Активний(авторизований) користувач:", activeUser); //!
-//   return (
-//     <div className={css.boxRegistrationIdentification}>
-//       {activeUser
-//         ?
-//         <h2 className={css.titleRegistrationIdentification}>
-//           Вітаю вас, <span className={css.titleUserRegistrationIdentification}>{activeUser?.userName}</span>
-//         </h2>
-//         :
-//         <h2 className={`${css.titleRegistrationIdentification} ${css.titleReminderRegistrationIdentification}`}>
-//           <i>Для здійснення покупок необхідно увійти до свого акаунту ⇒</i>
-//         </h2>
-//       }
-//       <div className={css.buttonBoxRegistrationIdentification}>
-
-//         {!activeUser &&
-//           <button
-//             className={`${css.buttonRegistrationIdentification} ${css.buttonRegistration}`}
-//             type="button"
-//             // onClick={() => console.log('Клік в "Registration"')}
-//             onClick={onClose}
-//             // onClick={(event) => onClose(event.currentTarget.textContent)}
-//           >
-//             Registration
-//           </button>
-//         }
-
-//         {!activeUser &&
-//           <button
-//             className={`${css.buttonRegistrationIdentification} ${css.buttonLogin}`}
-//             type="button"
-//             // onClick={() => console.log('Клік в "Login"')}
-//             onClick={onClose}
-//             // onClick={(event) => onClose(event.currentTarget.textContent)}
-//           >
-//             Login
-//           </button>
-//         }
-
-//         {activeUser &&
-//           <button
-//             className={`${css.buttonRegistrationIdentification} ${css.buttonSignOut}`}
-//             type="button"
-//             onClick={onSignOut}
-//           >
-//             SignOut
-//           </button>
-//         }
-
-//       </div>
-//     </div>
-//   )
-// };
